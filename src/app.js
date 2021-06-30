eb24c83d4c59bc28588addea522e8e8f9804150c
Bezaliel Silva <bcs5@cin.ufpe.br>
2021-06-30 19:16:16 -0300

    feat(app.js): adds some book routes

--- src/app.js
+++ src/app.js
@@ -5,12 +5,21 @@ const port = 3000
 
 const { Book } = require('./models');
 
-Book.create({ isbn: 289102, title: 'cHamlet', copies: 5 });
+//Book.create({ isbn: 289102, title: 'cHamlet', copies: 5 });
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/books', (req, res) => {
+  Book.findAll().then(books => {
+    res.send(books)
+  })
 })
+
+app.get('/book/:id', (req, res) => {
+  console.log(req.id);
+  Book.findByPk(req.params.id).then(book => {
+    res.send(book)
+  })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
