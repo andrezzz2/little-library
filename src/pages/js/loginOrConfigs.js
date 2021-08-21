55dddaf2f7f9dcb3cb247abb142e0c5cf4c89475
André <alfr2@cin.ufpe.br>
2021-08-21 02:37:40 -0300

    adicionando no componente tipo de usuario

--- src/pages/js/loginOrConfigs.js
+++ src/pages/js/loginOrConfigs.js
@@ -10,17 +10,17 @@ function Login(props) {
 
 function Configs(props) {
     return(
-        <div class="dropdown">
-            <button class="btn btn-outline-light dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fas fa-user-cog"></i>
+        <div className="dropdown">
+            <button className="btn btn-outline-light dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                <i className="fas fa-user-cog"></i>
             </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                <li><h6 class="dropdown-item">Logado como:<em> {username}, usuário</em></h6></li>  
-                <li><hr class="dropdown-divider"/></li>
-                <li><a class="dropdown-item" href="#">Meu Perfil</a></li>
-                <li><a class="dropdown-item" href="#">Notificações</a></li>
-                <li><hr class="dropdown-divider"/></li>
-                <li><a class="dropdown-item" href="#">Sair</a></li>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><h6 className="dropdown-item">Logado como:<em> {username}, {userType}</em></h6></li>  
+                <li><hr className="dropdown-divider"/></li>
+                <li><a className="dropdown-item" href="#">Meu Perfil</a></li>
+                <li><a className="dropdown-item" href="#">Notificações</a></li>
+                <li><hr className="dropdown-divider"/></li>
+                <li><a className="dropdown-item" href="#">Sair</a></li>
             </ul>
         </div>
     );
@@ -31,6 +31,10 @@ if(cookies.length==0){
     var username = cookies
                 .split('; ')
                 .find(row => row.startsWith('username='))
+                .split('=')[1];
+    var userType = cookies
+                .split('; ')
+                .find(row => row.startsWith('userType='))
                 .split('=')[1];
     ReactDOM.render(<Configs/>, document.querySelector('.loginOrConfigs'));
 }
